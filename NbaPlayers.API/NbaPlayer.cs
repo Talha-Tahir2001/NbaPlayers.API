188646010c9435256dd3b6146e085ac4afcf5c6a
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2024-09-01 00:13:55 +0500

    Add Project Files

---
+++ NbaPlayers.API/NbaPlayer.cs
@@ -0,0 +1,11 @@
+﻿namespace NbaPlayers.API
+{
+    public class NbaPlayer
+    {
+        public int Id { get; set; }
+        public string NickName { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Team { get; set; } = string.Empty;
+    }
+}
